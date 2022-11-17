1c6fef04bd53c7c94d40def633eb1a8c62e25919
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-17 14:24:42 +0100

    4 requests done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Acake!')
-})
+app.get('/', (req, res) => {res.send('Hello this is a GET REQUEST!')})
+app.post('/', (req, res) => {res.send('Hello this is a post REQUEST!')})
+app.put('/', (req, res) => {res.send('Hello this is a put REQUEST!')})
+app.delete('/', (req, res) => {res.send('Hello this is a delete REQUEST!')})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
