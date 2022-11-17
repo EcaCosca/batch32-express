094da96f86267f0d1e7646b047cd2841b89bff09
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-17 14:52:54 +0100

    dinamic done

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app
     .post((req, res) => {res.send('Hello this is a post REQUEST!')})
     .put((req, res) => {res.send('Hello this is a put REQUEST!')})
     .delete((req, res) => {res.send('Hello this is a delete REQUEST!')});
+    
+// redirect example
+    app.get('/getexample', (req,res) => {res.redirect('/')})
+
+//making it dynamic
+app.get('/users/:id', (req, res)=>{
+    console.log(req.params.id);
+    console.log(req.query.sort);
+    res.send(`req.params is id and the value is ${req.params.id}\n req.query is ${req.query.sort}`);
+}) 
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
