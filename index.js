f26f814711711e1e6cd694979798bacd66775b11
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-17 14:34:27 +0100

    implementing routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {res.send('Hello this is a GET REQUEST!')})
-app.post('/', (req, res) => {res.send('Hello this is a post REQUEST!')})
-app.put('/', (req, res) => {res.send('Hello this is a put REQUEST!')})
-app.delete('/', (req, res) => {res.send('Hello this is a delete REQUEST!')})
+app
+    .route('/')
+    .get((req, res) => {res.send('Hello this is a GET REQUEST!')})
+    .post((req, res) => {res.send('Hello this is a post REQUEST!')})
+    .put((req, res) => {res.send('Hello this is a put REQUEST!')})
+    .delete((req, res) => {res.send('Hello this is a delete REQUEST!')});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
